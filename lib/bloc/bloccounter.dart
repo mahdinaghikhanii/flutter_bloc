e8d1ab785b4f0caa80a1a4d6de3b067c771b552c
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-26 20:47:32 +0430

    crete in CounterBloc models

--- lib/bloc/bloccounter.dart
+++ lib/bloc/bloccounter.dart
@@ -1,14 +1,31 @@
 import 'dart:async';
 
-import 'package:bloc/bloc/blocevent.dart';
+import 'blocevent.dart';
 
 class CounterBloc {
   int _counter = 0;
   final StreamController<int> _counterStatestController =
       StreamController<int>();
-  StreamSink<int> get _intCounter => _counterStatestController.sink;
+  StreamSink<int> get _inCounter => _counterStatestController.sink;
   Stream<int> get counter => _counterStatestController.stream;
 
   final _counterEventController = StreamController<CounterEvent>();
   Sink<CounterEvent> get counterEventSink => _counterEventController.sink;
+
+  CounterBloc() {
+    _counterEventController.stream.listen(_mapEventToState);
+  }
+  void _mapEventToState(CounterEvent event) {
+    if (event == CounterEvent.incerment) {
+      _counter++;
+    } else {
+      _counter--;
+    }
+    _inCounter.add(_counter);
+  }
+
+  void dispose() {
+    _counterStatestController.close();
+    _counterEventController.close();
+  }
 }
