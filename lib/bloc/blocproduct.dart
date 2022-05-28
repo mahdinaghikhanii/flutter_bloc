7f40d8790fc22c09838c48ece1c6f79e380b6dab
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-28 18:23:52 +0430

    fixed problems

--- lib/bloc/blocproduct.dart
+++ lib/bloc/blocproduct.dart
@@ -1,30 +1,29 @@
-import 'package:blocs/bloc/blocstate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../model/product.dart';
+import 'blocstate.dart';
 
 abstract class BlocEvent {}
 
 class AddToCart extends BlocEvent {
-  Product? prd;
-  AddToCart({this.prd});
+  Product prd;
+  AddToCart({required this.prd});
 }
 
 class DelCart extends BlocEvent {
-  Product? prd;
-  DelCart({this.prd});
+  Product prd;
+  DelCart({required this.prd});
 }
 
 class ProductBloc extends Bloc<BlocEvent, BlocState> {
-  ProductBloc() : super(SuccessState(product: [])) {
-    on<BlocEvent>((event, emit) {});
-  }
+  ProductBloc() : super(SuccessState(product: []));
   List<Product> cartProducts = [];
 
+  @override
   Stream<BlocState> mapEventToState(BlocEvent event) async* {
     yield LoadingState();
     try {
-      if (event is AddToCart) cartProducts.add(event.prd!);
+      if (event is AddToCart) cartProducts.add(event.prd);
 
       if (event is DelCart) cartProducts.remove(event.prd);
 
